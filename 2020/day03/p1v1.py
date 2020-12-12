1550e359d9250ad5439f7f49e8576988b65273f8
Steve Nielsen <steve@nielsenhq.com>
2020-12-12 10:47:09 -0600

    Day 3 problem 1

---
+++ 2020/day03/p1v1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+import sys
+
+data = []
+for line in sys.stdin:
+    data.append(line.rstrip())
+
+right = 3
+down = 1
+tree = 0
+col = 0;
+
+for line in data[down::down]: # start on row 1, step 1 throughh array
+    col += right
+    if line[col % len(line)] == '#':
+        tree += 1
+
+print("trees encountered %s" % tree)
